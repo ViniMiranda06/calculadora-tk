0ed6fdf0b7722ad7071d20f89ddc2ef0e9e38dd1
Tobyene <gothbr58@gmail.com>
2025-06-05 10:21:13 -0300

    Implement. botão BIN e DEC + Correções no código

--- Calculadora.py
+++ Calculadora.py
@@ -11,6 +11,7 @@ def calculate(operation):
             num = int(eval(expression))
             expression = bin(num)[2:]
             label_text.set(expression)
+
         except:
             label_text.set("error")
             expression = ""
@@ -22,12 +23,11 @@ def calculate(operation):
         except:
             label_text.set("error")
             expression = ""
-        
     else:
-        if expression == "" and operation in ['+','-','*','/']:
+        if expression == "" and operation in ['+', '-', '*', '/']:
             return
         expression += operation
-        
+
     label_text.set(expression)
 
 def evaluate():
@@ -54,19 +54,17 @@ label.grid(row=0, column=0, columnspan=4)
 botoes = [
     ('7', 1, 0), ('8', 1, 1), ('9', 1, 2), ('C', 1, 3),
     ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('+', 2, 3),
-    ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3), 
-    ('0', 4, 0), ('.', 4, 1), ('B', 4, 2), ('*', 4, 3), 
-    ('/', 5, 0)
+    ('1', 3, 0), ('2', 3, 1), ('3', 3, 2), ('-', 3, 3),
+    ('0', 4, 0), ('.', 4, 1), ('B', 4, 2), ('*', 4, 3),
+    ('/', 5, 0),
+    ('Bin', 6, 0), ('Dec', 6, 1)
 ]
 
 for text, row, col in botoes:
-    if text == '=':
-        action = evaluate
-    else:
-        action = lambda x=text: calculate(x)
+    action = lambda x=text: calculate(x)
     btn = tk.Button(root, text=text, command=action, font=('Arial', 30), width=5, height=2)
     btn.grid(row=row, column=col)
-    
+
 btn_equal = tk.Button(root, text='=', command=evaluate, font=('Arial', 30), width=11, height=2)
 btn_equal.grid(row=5, column=1, columnspan=2)
 
